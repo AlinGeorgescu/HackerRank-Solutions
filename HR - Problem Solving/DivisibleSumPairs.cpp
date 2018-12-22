c54f691688c387dbefb0e7890035da397204f6dd
AlinGeorgescu <33759291+AlinGeorgescu@users.noreply.github.com>
2018-12-22 22:38:38 +0200

    Update DivisibleSumPairs.cpp

--- HR - Problem Solving/DivisibleSumPairs.cpp
+++ HR - Problem Solving/DivisibleSumPairs.cpp
@@ -10,13 +10,20 @@ using std::cout;
 using std::vector;
 
 int divisibleSumPairs(int n, int k, vector<int> array) {
-    int number = 0;
+    int number;
+    vector<int> freq(k, 0);
 
-    for (int i = 0; i < array.size(); ++i) {
-        for (int j = i + 1; j < array.size(); ++j) {
-            if ( (array[i] + array[j]) % k == 0 )
-                ++number;
-        }
+    for(int i = 0; i < n; ++i) {
+        ++freq[ array[i] % k ];
+    }
+
+    number = ( freq[0] * (freq[0] - 1) ) / 2;
+    for(int i = 1; i <= k / 2  && i != k - i; ++i) {
+        number += freq[i] * freq[k - i];
+    }
+
+    if(k % 2 == 0) {
+        number += ( freq[k / 2] * (freq[k / 2] - 1) ) / 2;
     }
 
     return number;
