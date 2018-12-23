23118155b6e449fb058c79f16a6a67e818ad0f26
AlinGeorgescu <33759291+AlinGeorgescu@users.noreply.github.com>
2018-12-23 18:10:46 +0200

    Create BonAppetit.cpp

---
+++ HR - Problem Solving/BonAppetit.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using std::cin;
+using std::cout;
+using std::vector;
+using std::accumulate;
+
+void bonAppetit(vector<int> bill, int k, int b) {
+    int sum = accumulate(bill.begin(), bill.end(), 0);
+    sum = (sum - bill[k]) / 2;
+
+    if (sum == b) {
+        cout << "Bon Appetit\n";
+    } else {
+        cout << b - sum << '\n';
+    }
+}
+
+int main(){
+    int n, k, b;
+    vector<int> bill;
+
+    cin >> n >> k;
+    bill.resize(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> bill[i];
+    }
+
+    cin >> b;
+
+    bonAppetit(bill, k, b);
+
+    return 0;
+}
